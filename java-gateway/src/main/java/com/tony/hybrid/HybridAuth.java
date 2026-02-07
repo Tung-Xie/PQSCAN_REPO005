f2a5fdcd0718525c47319dd6ace430c7888b70f4
dub dub Wubba lubba <tonyhsieh606@gmail.com>
2026-02-08 05:41:49 +0800

    feat: enrich hybrid risk algos

--- java-gateway/src/main/java/com/tony/hybrid/HybridAuth.java
+++ java-gateway/src/main/java/com/tony/hybrid/HybridAuth.java
@@ -1,15 +1,13 @@
 package com.tony.hybrid;
 import java.security.Signature;
+import java.security.KeyPairGenerator;
 
 public class HybridAuth {
-    public void processHybridLogin() throws Exception {
-        // [Critical] 混合模式測試 - 含有 RSA2048
-        String hybridKex = "MLKEM768_X25519";
-        String hybridSig = "MLDSA87_RSA2048";
-        
-        // 關鍵呼叫：這會讓掃描器偵測到使用了 RSA
-        Signature sig = Signature.getInstance("SHA256withRSA");
-        
-        System.out.println("Executing HybridAuth: " + hybridSig);
+    public void processHybrid() throws Exception {
+        // [Critical] 測試混合資產：同時出現 PQC 與 舊版 RSA
+        String hybridSig = "MLDSA87_RSA2048"; 
+        Signature.getInstance("SHA256withRSA"); // RSA 2048 標記
+        KeyPairGenerator.getInstance("ML-DSA-87"); // PQC 標記
+        System.out.println("Hybrid Mode: " + hybridSig);
     }
 }
